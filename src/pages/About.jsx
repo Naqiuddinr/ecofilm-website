0e4ef656902b840374ab34f0473fa799ee3f03d4
NaqiuddinR <mnaqiuddin.rsl@gmail.com>
2024-06-18 17:58:32 +0800

    added video category section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React, { forwardRef } from 'react'
 function About(props, ref) {
     return (
         <div ref={ref} className="container mx-auto p-8">
-            <h2 className="text-3xl font-bold mt-10 mb-4">About Us</h2>
+            <h2 className="text-3xl text-black font-bold mt-10 mb-4">About Us</h2>
             <p className="text-lg">
                 1914 translation by H. Rackham
                 "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import landingVideo from '../assets/home-landing-loop.mp4'
 import About from './About';
+import Section from './components/Section';
 
 export default function Home() {
 
@@ -19,6 +20,7 @@ export default function Home() {
                 </div>
             </div>
             <About ref={ref} />
+            <Section />
         </>
     )
 }
@@ -32,16 +34,16 @@ const DrawOutlineButton = ({ children, ...rest }) => {
             <span>{children}</span>
 
             {/* TOP */}
-            <span className="absolute left-0 top-0 h-[2px] w-0 bg-indigo-300 transition-all duration-100 group-hover:w-full" />
+            <span className="absolute left-0 top-0 h-[2px] w-0 bg-white transition-all duration-100 group-hover:w-full" />
 
             {/* RIGHT */}
-            <span className="absolute right-0 top-0 h-0 w-[2px] bg-indigo-300 transition-all delay-100 duration-100 group-hover:h-full" />
+            <span className="absolute right-0 top-0 h-0 w-[2px] bg-white transition-all delay-100 duration-100 group-hover:h-full" />
 
             {/* BOTTOM */}
-            <span className="absolute bottom-0 right-0 h-[2px] w-0 bg-indigo-300 transition-all delay-200 duration-100 group-hover:w-full" />
+            <span className="absolute bottom-0 right-0 h-[2px] w-0 bg-white transition-all delay-200 duration-100 group-hover:w-full" />
 
             {/* LEFT */}
-            <span className="absolute bottom-0 left-0 h-0 w-[2px] bg-indigo-300 transition-all delay-300 duration-100 group-hover:h-full" />
+            <span className="absolute bottom-0 left-0 h-0 w-[2px] bg-white transition-all delay-300 duration-100 group-hover:h-full" />
         </button>
     );
 };
